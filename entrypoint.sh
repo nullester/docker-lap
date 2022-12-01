e087c7319b9d4864c018f7293cfbcb66b5760a52
nullester <65467511+nullester@users.noreply.github.com>
2022-12-01 16:39:49 +0100

    User permissions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,26 @@
 #!/bin/bash
 
+echo && echo -e "\033[032mI AM \033[035m$( whoami )\033[032m!\033[0m" && echo
+
+su root -c "export HOME=/root"
+su docker -c "export HOME=/home/docker"
+
+echo -e "Starting service \033[032mapache2\033[0m"
 if [[ "$USER" != "root" ]]; then
-    sudo apachectl -D FOREGROUND
+    sudo service apache2 start
 else
-    apachectl -D FOREGROUND
+    service apache2 start
 fi
+echo
+
+echo -e "Starting service \033[032mphp${PHP_VERS}-fpm\033[0m"
+if [[ "$USER" != "root" ]]; then
+    sudo service php$PHP_VERS-fpm start
+else
+    service php$PHP_VERS-fpm start
+fi
+echo -e "\033[032mphp${PHP_VERS}-fpm\033[0m started"
+echo
+
+echo -e -n "Server \033[032m${HOSTNAME}\033[0m up and running!"
+tail -f /dev/null
